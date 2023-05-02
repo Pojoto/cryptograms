745202b6aee03e1964928f1fb2c7b7c16b8520ba
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-05-02 15:51:59 -0400

    made GUI look a little better - still needs work

--- src/main.py
+++ src/main.py
@@ -6,11 +6,26 @@ root = Tk()
 
 root.title("Cryptograms")
 
-root.configure(width=600, height=300, background='lightblue')
+root.configure(background='lightblue')
 
-text = "hello there"
+window_width = 600
+window_height = 400
 
-cryptogram = Cryptogram(root, text)
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width / 2 - window_width / 2)
+center_y = int(screen_height / 2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+plaintext = "hello there"
+ciphertext = "abccd eabgb"
+
+cryptogram = Cryptogram(root, ciphertext, plaintext)
 
 
 # entry_text = StringVar()
