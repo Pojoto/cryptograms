dcc004755b1e3f58814078a1bf2d71a5f07833b9
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-05-02 23:51:27 -0400

    added unit and entryunit subclass distinction

--- src/unit.py
+++ src/unit.py
@@ -8,17 +8,26 @@ class Unit:
         self.frame = Frame(window, width=30, height=100)
         self.frame.pack(side=LEFT, expand=True)
 
+        self.label = Label(self.frame, text=char)
+        self.label.pack()
+
+
+
+
+
+class EntryUnit(Unit):
+    
+    def __init__(self, window, char):
+
+        super().__init__(window, char)
+
         self.entry_text = StringVar()
 
         self.entry = Entry(self.frame, width=3, textvariable=self.entry_text)
         self.entry.pack(padx=5,pady=5)
-        self.label = Label(self.frame, text=char)
-        self.label.pack()
+
         self.entry_text.trace_add("write", lambda *args: self.character_limit(self.entry_text))
     
     def character_limit(self, entry_text):
         if len(entry_text.get()) > 0:
             entry_text.set(entry_text.get()[-1])
-
-
-#TODO: each unit should have a stringvar, entry, and letter field all in a packed canvas
