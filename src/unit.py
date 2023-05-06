26670ca4c6905ef1366e62608fbbd2b59f8b6567
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-05-06 16:48:26 -0400

    added spacebar next feature

--- src/unit.py
+++ src/unit.py
@@ -1,12 +1,20 @@
 from tkinter import *
+from random import randint
 
 class Unit:
 
     def __init__(self, window, char):
         self.char = char
         
-        self.frame = Frame(window, width=30, height=100)
-        self.frame.pack(side=LEFT, expand=True)
+        self.frame = Frame(window, width=30, height=0)#, background="black")
+
+        # if randint(0, 10) < 5:
+        #     self.frame.pack(side=LEFT)
+        # else:
+        #     self.frame.pack()
+
+
+        self.frame.pack(side=LEFT)#, expand=True)
 
         self.label = Label(self.frame, text=char)
         self.label.pack()
@@ -23,8 +31,9 @@ class EntryUnit(Unit):
 
         self.entry_text = StringVar()
 
-        self.entry = Entry(self.frame, width=3, textvariable=self.entry_text)
+        self.entry = Entry(self.frame, width=2, textvariable=self.entry_text)
         self.entry.pack(padx=5,pady=5)
+        self.entry.lift()
 
         self.entry_text.trace_add("write", lambda *args: self.character_limit(self.entry_text))
     
