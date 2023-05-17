a9d4eb47688724ffd28513b67f5b2f2360485171
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-05-16 22:12:06 -0400

    changed to fullscreen startup

--- src/main.py
+++ src/main.py
@@ -11,19 +11,24 @@ root.title("Cryptograms")
 
 root.configure(background='lightblue')
 
-window_width = 1200
-window_height = 800
+root.state("zoomed")
 
-# get the screen dimension
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+#root.attributes("-fullscreen", True)
 
-# find the center point
-center_x = int(screen_width / 2 - window_width / 2)
-center_y = int(screen_height / 2 - window_height / 2)
 
-# set the position of the window to the center of the screen
-root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+# window_width = 1200
+# window_height = 800
+
+# # get the screen dimension
+# screen_width = root.winfo_screenwidth()
+# screen_height = root.winfo_screenheight()
+
+# # find the center point
+# center_x = int(screen_width / 2 - window_width / 2)
+# center_y = int(screen_height / 2 - window_height / 2)
+
+# # set the position of the window to the center of the screen
+# root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
 with open("quotes.txt", "r") as quotes_file:
     global quotes
