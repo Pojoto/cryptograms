e99ab03ba8a43b72aafeaedfd2473b2d7ce23437
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-05-22 20:57:36 -0400

    fixed key_dict syncing and check answer now uses keydict

--- src/mainframe.py
+++ src/mainframe.py
@@ -13,21 +13,18 @@ def encrypt_and_key(plaintext):
 
     key_dict = {}
 
+    main_set = alpha_set.copy()
+
     for letter in plaintext:
         if letter in alpha_set and letter not in key_dict:
-            key_dict[letter] = ""
-
-    #
-    main_set = alpha_set.copy()
-    for plainkey in key_dict:
-        temp_set = main_set.copy()
-        try:
-            temp_set.remove(plainkey)
-        except:
-            pass
-        ciphertext = choice(tuple(temp_set))
-        main_set.remove(ciphertext)
-        key_dict[plainkey] = ciphertext
+            temp_set = main_set.copy()
+            try:
+                temp_set.remove(letter)
+            except:
+                pass
+            ciphertext = choice(tuple(temp_set))
+            main_set.remove(ciphertext)
+            key_dict[letter] = ciphertext
 
     ciphertext = ""
     for ch in plaintext:
@@ -35,8 +32,10 @@ def encrypt_and_key(plaintext):
             ciphertext += key_dict[ch]
         else:
             ciphertext += ch
-    print(ciphertext)
-    return ciphertext#, key
+
+    key_dict = {key_dict[k]:k for k in key_dict}
+
+    return ciphertext, key_dict
 
 
 class MainFrame:
@@ -64,7 +63,7 @@ class MainFrame:
     def new_quote(self):
         quote = choice(self.quotes)
         self.plaintext = quote
-        self.ciphertext = encrypt_and_key(quote)
+        self.ciphertext, self.key_dict = encrypt_and_key(quote)
 
     def clear(self):
         self.cryptogram.clear_answer()
@@ -72,17 +71,13 @@ class MainFrame:
     def enter_pressed(self, event):
         print(time.time())
         self.check_answer()
-
+            
     def check_answer(self):
 
-        user_answer = self.cryptogram.get_answer()
+        print(self.cryptogram.key_dict)
+        print(self.key_dict)
 
-        #all characters not to be read from the plaintext - so punctuation and space
-        char_filter = string.punctuation + ' ' 
-        
-        plaintext_no_punc = self.plaintext.translate(str.maketrans('', '', char_filter))
-
-        if user_answer == plaintext_no_punc.upper():
+        if self.cryptogram.key_dict == self.key_dict:
             print("CORRECT!")
             self.cryptogram.self_destruct()
             self.new_quote()
@@ -91,8 +86,6 @@ class MainFrame:
             self.cryptogram.frame.pack()
         else:
             print("WRONG!")
-            
-    
 
     
     #TODO: make the cryptogram class have its own frame
